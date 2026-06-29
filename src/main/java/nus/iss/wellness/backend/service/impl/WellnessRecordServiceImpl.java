bc38afeb8b2b4c504a9524a5f166e6bcd7f28927
pwtansg <pwtan.edu@gmail.com>
2026-06-29 09:28:11 +0800

    Add edit/update endpoint for wellness records

--- src/main/java/nus/iss/wellness/backend/service/impl/WellnessRecordServiceImpl.java
+++ src/main/java/nus/iss/wellness/backend/service/impl/WellnessRecordServiceImpl.java
@@ -6,7 +6,9 @@ import nus.iss.wellness.backend.model.WellnessRecord;
 import nus.iss.wellness.backend.repository.WellnessRecordRepository;
 import nus.iss.wellness.backend.service.WellnessRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class WellnessRecordServiceImpl implements WellnessRecordService {
@@ -26,6 +28,27 @@ public class WellnessRecordServiceImpl implements WellnessRecordService {
         record.setRecordDate(request.getRecordDate());
         record.setNotes(request.getNotes());
         return WellnessRecordResponse.from(wellnessRecordRepository.save(record));
+    }
+
+    @Override
+    public WellnessRecordResponse updateRecord(Long recordId, WellnessRecordRequest request) {
+
+        WellnessRecord record = wellnessRecordRepository.findById(recordId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Record not found"));
+
+        // record.setUserId(record.getUserId());
+        record.setCategory(request.getCategory());
+        record.setValue(request.getValue());
+        record.setCaloriesBurned(request.getCaloriesBurned());
+        record.setUnit(request.getUnit());
+        record.setDurationMinutes(request.getDurationMinutes());
+        record.setRecordDate(request.getRecordDate());
+        record.setNotes(request.getNotes());
+
+        WellnessRecord updatedRecord = wellnessRecordRepository.save(record);
+
+        return WellnessRecordResponse.from(updatedRecord);
     }
 }
 //
