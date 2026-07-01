7437f6e1f88c9b2c187262103af16e1a48be29ef
Htet Nandar <htetnandar.282198@gmail.com>
2026-07-01 18:38:09 +0800

    Implement ChatMessageRepository and ChatSessionRepository with JPA support

--- src/main/java/nus/iss/wellness/backend/repository/ChatMessageRepository.java
+++ src/main/java/nus/iss/wellness/backend/repository/ChatMessageRepository.java
@@ -1,7 +1,16 @@
 package nus.iss.wellness.backend.repository;
 
+import nus.iss.wellness.backend.model.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+/**
+ *  Author: Htet Nandar
+ */
 @Repository
-public interface ChatMessageRepository {
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    // All messages in a session, ordered chronologically
+    List<ChatMessage> findBySessionIdOrderByCreatedAtAsc(Long sessionId);
 }
